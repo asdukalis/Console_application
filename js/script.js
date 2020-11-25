58176d356a09dc444c8dfe5954ad8e5e7d260caf
Andrey <asdukalis@gmail.com>
2020-11-25 13:44:53 +0200

    Цикл for

--- js/script.js
+++ js/script.js
@@ -13,11 +13,28 @@ const personalMovieDB = {
   privat: false,
 };
 
-const a = prompt('Один из последних просмотренных фильмов?', '');
-const b = prompt('На сколько оцнили его?', '');
-const c = prompt('Один из последних просмотренных фильмов?', '');
-const d = prompt('На сколько оцнили его?', '');
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
+// const a = prompt('Один из последних просмотренных фильмов?', '');
+// const b = prompt('На сколько оцнили его?', '');
+// const c = prompt('Один из последних просмотренных фильмов?', '');
+// const d = prompt('На сколько оцнили его?', '');
+// personalMovieDB.movies[a] = b;
+// personalMovieDB.movies[c] = d;
 
 console.log(personalMovieDB);
+
+for (let i = 0; i < 2; i++) {
+  const a, b;
+  const a = prompt('Один из последних просмотренных фильмов?', '');
+  if (a == '') {
+    alert('Ответте на вопрос !!!');
+    prompt('Один из последних просмотренных фильмов?', '');
+  } else {
+    const b = prompt('На сколько оцнили его?', '');
+  }
+  if (b == '') {
+    alert('Ответте на вторй вопрос !!!');
+    prompt('Один из последних просмотренных фильмов?', '');
+  } else {
+    personalMovieDB.movies[a] = b;
+  }
+}
