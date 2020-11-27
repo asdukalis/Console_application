b9eb748785b84682f9295e8e1efa8b7dd19ff674
Andrey <asdukalis@gmail.com>
2020-11-27 16:42:43 +0200

    дестркутуризация

--- js/functions.js
+++ js/functions.js
@@ -25,30 +25,73 @@
 // str.indexOf(substr, pos)
 // str.lastIndexOf(substr, position)
 // str.slice(start [, end]) // Возвращает часть строки от start до (не включая) end.
-// str.substring(start [, end]) // Возвращает часть строки между start и end. Это — почти то же, что и slice, но можно задавать start больше end.
+// str.substring(start [, end]) -
+// Возвращает часть строки между start и end. Это — почти то же, что и slice, но можно задавать start больше end.
 
 // const test = '12px';
 // console.log(parseInt(test));
 
-function first() {
-  // Do something
-  setTimeout(function () {
-    console.log(1);
-  }, 500);
+// function first() {
+//   // Do something
+//   setTimeout(function () {
+//     console.log(1);
+//   }, 500);
+// }
+
+// function second() {
+//   console.log(2);
+// }
+
+// first();
+// second();
+
+// function learnJS(lang, callback) {
+//   console.log(`я учу: ${lang}`);
+//   callback();
+// }
+
+// learnJS('JavaScript', function () {
+//   console.log('Я прошел этот урок!');
+// });
+
+const options = {
+  name: 'Andrey',
+  width: 1024,
+  height: 1024,
+  colors: {
+    border: 'black',
+    bg: 'red',
+  },
+  makeTest: function () {
+    console.log('Test');
+  },
+};
+
+options.makeTest();
+
+// дестркутуризация
+const { border, bg } = options.colors;
+console.log(border);
+
+console.log(Object.keys(options).length);
+
+console.log(options.name);
+console.log(options.colors.bg);
+
+// delete options.name;
+
+console.log(options);
+
+let counter = 0;
+for (let key in options) {
+  if (typeof options[key] === 'object') {
+    for (let i in options[key]) {
+      console.log(`Свойство ${i} имеет значение ${options[key][i]}`);
+      counter++;
+    }
+  } else {
+    console.log(`Свойство ${key} имеет значение ${options[key]}`);
+    counter++;
+  }
 }
-
-function second() {
-  console.log(2);
-}
-
-first();
-second();
-
-function learnJS(lang, callback) {
-  console.log(`я учу: ${lang}`);
-  callback();
-}
-
-learnJS('JavaScript', function () {
-  console.log('Я прошел этот урок!');
-});
+console.log(counter);
